4d681f152413581fd2e41612ca8cae96580a8b38
Prasurjya Sarma <82230151+Prasurjyasarma@users.noreply.github.com>
2024-01-25 16:40:16 +0530

    1.1

---
+++ Main-app/details-content/detailscontainer.js
@@ -0,0 +1,57 @@
+//! Wait for the DOM content to be fully loaded before executing the script
+document.addEventListener("DOMContentLoaded", function () {
+  const closeButton = document.getElementById("close");
+  const preLoader = document.getElementById("preloader");
+  window.addEventListener("load", () => {
+    preLoader.style.display = "none";
+  });
+  //! Retrieve query parameters from the URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const productName = urlParams.get("name");
+  const productImageUrl = urlParams.get("imageUrl");
+  const productPrice = urlParams.get("price");
+  const productDetails = urlParams.get("details");
+
+  //! Get the container where the details will be displayed
+  const detailsContainer = document.getElementById("detailsContainer");
+
+  //! Create a container for images
+  const imagesContainer = document.createElement("div");
+
+  //! Create and append three image elements with the same URL (consider changing this logic)
+  for (let i = 0; i < 4; i++) {
+    const detailsImage = document.createElement("img");
+    detailsImage.src = productImageUrl;
+    imagesContainer.appendChild(detailsImage);
+  }
+
+  //! Create and set the title element with age, breed, and location information
+  const ageAndBreedTitle = document.createElement("h2");
+  ageAndBreedTitle.textContent = `${productName}`;
+
+  //! Create and set a paragraph element with a description of the dog
+  const detailsParagraph = document.createElement("p");
+  detailsParagraph.textContent = `${productDetails}`;
+  //! Create and set the title element with the dog's name
+  const detailsTitle = document.createElement("h3");
+  detailsTitle.textContent = ` Price : $ ${productPrice}`;
+
+  //! Create a button element for adoption
+  const detailsButton = document.createElement("button");
+  detailsButton.textContent = "Buy now";
+
+  //! Append the created elements to the detailsContainer
+  detailsContainer.appendChild(imagesContainer);
+  detailsContainer.appendChild(ageAndBreedTitle);
+  detailsContainer.appendChild(detailsTitle);
+  detailsContainer.appendChild(detailsParagraph);
+  detailsContainer.appendChild(detailsButton);
+
+  detailsButton.addEventListener("click", () => {
+    window.location.href = "./form-content/form-contetnt.html";
+  });
+
+  closeButton.addEventListener("click", () => {
+    window.location.href = "../main-app.html";
+  });
+});
---
+++ Main-app/main-app.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the container element where the cards will be added
+  const preLoader = document.getElementById("preloader");
+  const cardContainer = document.getElementById("cardContainer");
+
+  // Function to hide the preloader
+  window.addEventListener("load", () => {
+    preLoader.style.display = "none";
+  });
+
+  // Fetch data from the JSON file
+  fetch("../data.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      // Iterate through each item in the JSON data
+      jsonData.forEach((item) => {
+        // Create a card element
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const details = document.createElement("p");
+        details.textContent = item.details;
+        // card.appendChild(details);
+
+        // Create an image element and set its source
+        const image = document.createElement("img");
+        image.src = item.imageUrl;
+        card.appendChild(image);
+
+        // Create a title element and set its text content
+        const title = document.createElement("h2");
+        title.textContent = item.name;
+        card.appendChild(title);
+
+        // Create a price element and set its text content
+        const price = document.createElement("p");
+        price.textContent = `Price: $${item.price}`;
+        card.appendChild(price);
+
+        // Append the card to the card container
+        cardContainer.appendChild(card);
+
+        // Add a click event listener to each card
+        card.addEventListener("click", function () {
+          // Call the navigateToDetailsPage function when a card is clicked
+          navigateToDetailsPage(item);
+        });
+      });
+    })
+    .catch((error) => console.error("Error", error));
+
+  // Function to navigate to the details page with item information
+  function navigateToDetailsPage(item) {
+    // Create a query string with item information
+    const queryParams = `?name=${item.name}&imageUrl=${item.imageUrl}&price=${
+      item.price
+    }&details=${encodeURIComponent(item.details)}`;
+
+    // Set the window location to the details page with the query parameters
+    window.location.href = `./details-content/detailscontainer.html${queryParams}`;
+  }
+});
